58f54e95d5b6618aaee92c947c68c0f95948ef69
Alan Cortes <acortes@Lap-2.local>
2025-07-13 11:01:01 -0400

    Portfolio Refactor - Switched to JavaScript
    Updated UI, Experience, and utilized GitHub's API

---
+++ src/components/nav/bottomNavbar.jsx
@@ -0,0 +1,18 @@
+import {ThemeProvider, Tab, Tabs} from '@mui/material';
+import './styles/bottomNavbar.css';
+
+export default function BottomNavBar(props) {
+    return (
+        <div id="bottom-nav">
+            <ThemeProvider theme={props.navTheme}>
+                <Tabs  
+					value={props.currTab} 
+					onChange={(event, val) => props.setTab(val)}>
+                    {Object.keys(props.tabs).map((tab, index) => {
+                        return <Tab key={index} value={tab} icon={props.tabs[tab].icon} />
+                    })}
+				</Tabs>
+            </ThemeProvider>
+        </div>
+    )
+}
